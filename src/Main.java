f079e3cdd7b875539e3d53f884461c949de9c65f
dmarcha <dmarcha@cpp.edu>
2023-11-29 18:19:56 -0800

    implemented the testing method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,123 @@
 import java.util.Arrays;
+import java.util.Random;
+
+class Algo1 {
+    private int[] arr; // Array to be sorted
+    int kElement; // To store the kth smallest element
+
+    // Constructor to initialize the array and find the kth element
+    public Algo1(int[] arr, int k) {
+        this.arr = arr;
+        sort(this.arr, 0, this.arr.length - 1); // Sort the array
+        this.kElement = arr[k-1]; // Assign the kth smallest element after sorting
+    }
+
+    // Getter method for kth element
+    public int getkElement() {
+        return kElement;
+    }
+
+    // Merge function used in Merge Sort algorithm
+    private void merge(int[] arr, int left, int middle, int right) {
+        // Calculate the sizes of two subarrays to be merged
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        // Create temporary arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, middle + 1, R, 0, n2);
+
+        // Merge the temporary arrays back into the original array
+        int i = 0, j = 0; // Initial indexes of first and second subarrays
+        int k = left; // Initial index of merged subarray
+
+        // Merge process
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] and R[] if any
+        System.arraycopy(L, i, arr, k, n1 - i);
+        System.arraycopy(R, j, arr, k, n2 - j);
+    }
+
+    // Main function that sorts arr[l..r] using merge()
+    private void sort(int[] arr, int left, int right) {
+        if (left < right) {
+            // Find the middle point to divide the array into two halves
+            int middle = left + (right - left) / 2;
+
+            // Sort first and second halves
+            sort(arr, left, middle);
+            sort(arr, middle + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, middle, right);
+        }
+    }
+}
+
+class QuickSelect {
+    // Main function of QuickSelect algorithm to find the kth smallest element.
+    // It works similar to QuickSort but only processes one side of the pivot each time.
+    public static int quickSelect(int[] arr, int low, int high, int k) {
+        // If the subarray has only one element, return that element.
+        if (low == high) {
+            return arr[low];
+        }
+
+        // Partition the array and get the index of the pivot element.
+        int pivotIndex = partition(arr, low, high);
+
+        // If the pivot position is the same as k, we have found the kth smallest element.
+        if (k == pivotIndex) {
+            return arr[k];
+        } else if (k < pivotIndex) {
+            // If k is less than the pivot index, then the kth smallest element is in the left subarray.
+            return quickSelect(arr, low, pivotIndex - 1, k);
+        } else {
+            // If k is more than the pivot index, then the kth smallest element is in the right subarray.
+            return quickSelect(arr, pivotIndex + 1, high, k);
+        }
+    }
+
+    // Partition function used in QuickSort and QuickSelect algorithms.
+    // It arranges elements around a pivot such that elements smaller than pivot
+    // are on the left and larger ones are on the right.
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // Choosing the last element as the pivot
+        int i = low - 1; // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot) {
+                i++; // Increment the index of smaller element
+                swap(arr, i, j); // Swap the elements
+            }
+        }
+        swap(arr, i + 1, high); // Swap the pivot element with the element at i + 1
+        return i + 1; // Return the partitioning index
+    }
+
+    // Utility function to swap two elements in the array
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
 
 class MedianOfMedians {
     // Main function to find the kth smallest element in a given array.
@@ -99,18 +218,71 @@ class MedianOfMedians {
 }
 
 public class Main {
+    // Generates an array of random integers of a given size
+    static int[] generateRandomArray(int size) {
+        int[] arr = new int[size];
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(100); // Assuming the range of numbers is 0-99
+        }
+        return arr;
+    }
+    // Prints the array
+    static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
+        int maxArraySize = 5000;
+        int numberOfRuns = 10;
 
-        // Example array
-        int[] arr = {7, 10, 4, 3, 20, 15};
+        // Arrays to store average runtimes
+        double[] avgTimeMedianOfMedians = new double[maxArraySize];
+        double[] avgTimeAlgo1 = new double[maxArraySize];
+        double[] avgTimeQuickSelect = new double[maxArraySize];
 
-        // kth smallest element to find
-        int k = 3; // For example, find the 3rd smallest element
+        for (int size = 1; size <= maxArraySize; size++) {
+            long totalTimeMedianOfMedians = 0;
+            long totalTimeAlgo1 = 0;
+            long totalTimeQuickSelect = 0;
 
-        // Call the method and store the result
-        int kthSmallest = MedianOfMedians.getKth(arr,0, arr.length - 1, k);
+            for (int run = 0; run < numberOfRuns; run++) {
+                int[] arr = generateRandomArray(size);
+                int k = new Random().nextInt(size) + 1;
 
-        // Output the result
-        System.out.println("The " + k + "th smallest element is: " + kthSmallest);
+                // MedianOfMedians
+                long startTime = System.nanoTime();
+                MedianOfMedians.getKth(arr.clone(), 0, arr.length - 1, k);
+                totalTimeMedianOfMedians += System.nanoTime() - startTime;
+
+                // Algo1
+                startTime = System.nanoTime();
+                new Algo1(arr.clone(), k).getkElement();
+                totalTimeAlgo1 += System.nanoTime() - startTime;
+
+                // QuickSelect
+                startTime = System.nanoTime();
+                QuickSelect.quickSelect(arr.clone(), 0, arr.length - 1, k - 1);
+                totalTimeQuickSelect += System.nanoTime() - startTime;
+            }
+
+            avgTimeMedianOfMedians[size - 1] = (double)totalTimeMedianOfMedians / numberOfRuns;
+            avgTimeAlgo1[size - 1] = (double)totalTimeAlgo1 / numberOfRuns;
+            avgTimeQuickSelect[size - 1] = (double)totalTimeQuickSelect / numberOfRuns;
+        }
+
+        // Print average runtimes for each algorithm
+        System.out.println("Average runtimes (in nanoseconds):");
+        for (int size = 1; size <= maxArraySize; size++) {
+            System.out.println("Array Size: " + size);
+            System.out.println("MedianOfMedians Average runtimes (in nanoseconds): " + avgTimeMedianOfMedians[size - 1]);
+            System.out.println("Algo1 Average runtimes (in nanoseconds): " + avgTimeAlgo1[size - 1]);
+            System.out.println("QuickSelect Average runtimes (in nanoseconds): " + avgTimeQuickSelect[size - 1]);
+            System.out.println("---------------------------------");
+        }
     }
 }
